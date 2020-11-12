0f861fb825bb443aec344ad2c7ed3e47c705c054
rain <15921316892@163.com>
2020-11-12 19:08:11 +0800

    baseHolder

--- baseLib/src/main/java/com/rain/baselib/holder/BaseRecHolder.kt
+++ baseLib/src/main/java/com/rain/baselib/holder/BaseRecHolder.kt
@@ -1,5 +1,6 @@
 package com.rain.baselib.holder
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,8 +14,9 @@ import kotlinx.android.extensions.LayoutContainer
  *  Create by rain
  *  Date: 2020/11/6
  */
-abstract class BaseRecHolder<T>(@LayoutRes layoutResId: Int, parent: ViewGroup, private var variableId: Int) : RecyclerView.ViewHolder(DataBindingUtil.inflate<ViewDataBinding>(LayoutInflater.from(parent.context), layoutResId, parent, false).root), LayoutContainer {
+abstract class BaseRecHolder<T>(@LayoutRes layoutResId: Int, parent: ViewGroup, private var variableId: Int = -1) : RecyclerView.ViewHolder(DataBindingUtil.inflate<ViewDataBinding>(LayoutInflater.from(parent.context), layoutResId, parent, false).root), LayoutContainer {
 	override val containerView: View = itemView
+	protected val mContext:Context = itemView.context
 	open fun setData(model: T, position: Int) {
 		if (variableId != -1) {
 			val dataBind = DataBindingUtil.getBinding<ViewDataBinding>(itemView)
