0b6138fc78156003ecb22184e443538afae604fd
rain <15921316892@163.com>
2020-12-14 10:10:55 +0800

    clear

--- app/src/main/java/com/example/common/MainActivity.kt
+++ app/src/main/java/com/example/common/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.common
 
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.example.common.databinding.ActivityMainBinding
 import com.example.common.viewModel.MainViewModel
 import com.rain.baselib.activity.BaseDataBindActivity
@@ -11,19 +10,23 @@ import com.rain.baselib.common.startAc
 import com.says.common.file.PushFileManager
 
 class MainActivity : BaseDataBindActivity<ActivityMainBinding>() {
-    override val layoutResId = R.layout.activity_main
-    override val viewModel by viewModels<MainViewModel>()
-    override val variableId = BR.mainId
-    override fun onResume() {
-        super.onResume()
-        val pushMap = PushFileManager.pushMap
-        Log.d("pushTag", "pushMap:${pushMap.size}")
-    }
-
-    override fun initEvent() {
-        super.initEvent()
-        viewBind.tvStart.singleClick {
-            startAc<Demo2DataBindActivity>()
-        }
-    }
+	override val layoutResId = R.layout.activity_main
+	override val viewModel by viewModels<MainViewModel>()
+	override val variableId = BR.mainId
+	override fun onResume() {
+		super.onResume()
+		val pushMap = PushFileManager.pushMap
+		Log.d("pushTag", "pushMap:${pushMap.size}")
+	}
+	
+	override fun initEvent() {
+		super.initEvent()
+		viewBind.tvStart.singleClick {
+			startAc<DemoActivity>()
+		}
+		viewBind.tvEnd.singleClick {
+			startAc<Demo2DataBindActivity>()
+		}
+		
+	}
 }
